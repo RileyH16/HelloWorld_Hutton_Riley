d382adb0aba6e197611d6026073eb81f4b3f5c4f
RileyH16 <114522081+RileyH16@users.noreply.github.com>
2023-02-23 13:07:05 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// System namespace needed to use Console Class.
+using System;
+
+// My collection of classes for this project.
+namespace HelloWorld_Hutton_Riley
+{
+    // Main class for our program
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks the user for their first name
+            Console.WriteLine("What is your first name?");  
+            // Assigns user input to firstName variable
+            string firstName = Console.ReadLine();     
+            // Asks the user for their last name
+            Console.WriteLine("What is your last name?"); 
+            // Assigns user input to lastName variable
+            string lastName = Console.ReadLine();     
+            
+            // Prints a welcome message with firstName and lastName
+            Console.WriteLine($"Hello, {firstName} {lastName}!"); 
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld_Hutton_Riley
+ 
